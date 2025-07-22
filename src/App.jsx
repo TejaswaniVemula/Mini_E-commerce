92ab407c8c0254c74c05f6292790d349e427a8b1
TejaswaniVemula <tejaswanivemula73@gmail.com>
2025-07-22 20:25:52 +0530

    Add Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="app-container">
         <h1 className="app-title"> Mini E-Commerce</h1>
         <p className="app-subtitle">Explore our amazing products below.</p>
+        <ProductList />
       </div>
     </>
   );
